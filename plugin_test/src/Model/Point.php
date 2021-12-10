8d71f5092026dc86d6a0a448113154694befcea2
이세진 <41162249+vision20400@users.noreply.github.com>
2021-12-10 15:12:53 +0900

    Delete plugin_test directory

--- plugin_test/src/Model/Point.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace JTaylor\PluginTest\XePlugin\Model;
-
-use Xpressengine\Database\Eloquent\DynamicModel;
-
-class Point extends DynamicModel
-{
-    protected $primaryKey = 'userId';
-}
--- plugin_test/src/Model/PointLog.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace JTaylor\PluginTest\XePlugin\Model;
-
-use Xpressengine\Database\Eloquent\DynamicModel;
-
-class PointLog extends DynamicModel
-{
-    public $timestamps = false;
-}
